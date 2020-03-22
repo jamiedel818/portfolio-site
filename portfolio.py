d27192dd392b96937248d9ebeeb0d232a3d578dd
Jamie Del Prete <jamiedel818@my.uri.edu>
2020-03-22 01:49:17 -0400

    added new routes

--- portfolio.py
+++ portfolio.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 #'flask run' in the terminal will run the final version
 #'./portfolio will run in degbug mode'
 app = Flask(__name__)
 
+projects = [
+    {
+        'title': 'covid-19 tracker',
+        'datePublished': '05/05/20',
+        'author': 'jamiedel818',
+        'description': 'description',
+        'link': 'www.github.com'
+        },
+    {
+        'title': 'directory operations',
+        'datePublished': '05/05/20',
+        'author': 'jamiedel818',
+        'description': 'description',
+        'link': 'www.github.com'
+    },
+    {
+        'title': 'personal website',
+        'datePublished': '05/05/20',
+        'author': 'jamiedel818',
+        'description': 'description',
+        'link': 'www.github.com'
+    }
+]
+
 @app.route('/')
-@app.route('home')
+@app.route('/home')
 def home():
-    return '<h1>Homepage</h1>'
+    return render_template('home.html', projects=projects, title="Home")
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 #This is to get live updates-- debugger mode
 if __name__ == '__main__':
